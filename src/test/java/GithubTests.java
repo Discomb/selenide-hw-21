d1e613b027b0c94c9b99e5fae61848c4c9e23df1
Nikolai Malygin <n.p.malygin@gmail.com>
2023-07-26 11:15:52 +0300

    Minor fix

--- src/test/java/GithubTests.java
+++ src/test/java/GithubTests.java
@@ -14,7 +14,6 @@ public class GithubTests {
         baseUrl="https://github.com/";
         Configuration.browserSize="1920x1080";
         Configuration.pageLoadStrategy="eager";
-        Configuration.holdBrowserOpen = true;
     }
 
     @Test
